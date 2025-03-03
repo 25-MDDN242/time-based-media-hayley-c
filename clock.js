123f75c446eff6b1774bb951d1f2332a18bf4cbc
hayley-c <hj.chung24@gmail.com>
2025-03-04 11:21:15 +1300

    Week 2 Tuesday Studio

--- clock.js
+++ clock.js
@@ -1,6 +1,8 @@
 /*
  * use p5.js to draw a clock on a 960x500 canvas
  */
+
+
 function draw_clock(obj) {
   // draw your own clock here based on the values of obj:
   //    obj.hours goes from 0-23
@@ -12,32 +14,72 @@ function draw_clock(obj) {
   //        = 0 if the alarm is currently going off
   //        > 0 --> the number of seconds until alarm should go off
 
-  background(195, 201, 217);
+  background(141, 142, 143);
+
   push();
   translate(width/2, height/2);
-  ellipseMode(CENTER);
   noFill();
+  strokeWeight(1)
+  stroke(75, 77, 89);
+  ellipse(0, 10, 610, 310);
+  ellipse(0, 10, 590, 290);
+  strokeWeight(10)
+  stroke(93, 95, 110);
+  ellipse(0, 10, 600, 300);
   ellipse(0, 0, 600, 300);
-  beginShape();
-  vertex(-300, 50);
-  bezierVertex(-275, 225, 275, 225, 300, 50)
-  endShape();
+  ellipse(0, -10, 600, 300);
+  strokeWeight(1)
+  stroke(75, 77, 89);
+  ellipse(0, -10, 590, 290);
+  ellipse(0, -10, 610, 310);
   pop();
 
+  centreFlower();
 
+}
 
-  // background(50); //  beige
-  // fill(200); // dark grey
-  // textSize(40);
-  // textAlign(CENTER, CENTER);
-  // text("YOUR MAIN CLOCK CODE GOES HERE", width / 2, 200);
+function centreFlower() {
+  colorMode(RGB, 255, 255, 255, 1);
+  ellipseMode(CORNER);
+  angleMode(DEGREES);
+  // let petalLength = map(obj.seconds, 0, 359, 0, 30)
+  push();
+  noStroke();
+  fill(210, 231, 250, 0.2);
+  translate(width/2, height/2);
+  for (let i = 0; i <= 30; i++) {
+    rotate(12);
+    ellipse(0, 0, 25, 150);
 
+    // if(petalLength > 30) {
+    //   petalLength = 30
+    // }
+  
+  }
+  fill(239, 230, 252);
+  circle(0, 0, 25);
+  pop();
+}
 
-  // fill(249, 140, 255);// pink
-  // ellipse(width / 3, 350, 150);
-  // fill(140, 255, 251) // blue
-  // ellipse(width / 2, 350, 150);
-  // fill(175, 133, 255); // purple
-  // ellipse(width / 3 * 2, 350, 150);
+function miniFlower() {
+  colorMode(RGB, 255, 255, 255, 1);
+  ellipseMode(CENTER);
+  angleMode(DEGREES);
+  // let petalLength = map(obj.seconds, 0, 359, 0, 30)
+  push();
+  noStroke();
+  fill(210, 231, 250, 0.2);
+  translate(width/2, height/2);
+  for (let i = 0; i <= 12; i++) {
+    rotate(30);
+    ellipse(0, 0, 5,0+frameCount*0.05);
 
+    // if(petalLength > 30) {
+    //   petalLength = 30
+    // }
+  
+  }
+  fill(239, 230, 252);
+  circle(0, 0, 5);
+  pop();
 }
